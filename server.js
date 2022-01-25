96a1aee185627dc7f3e816c124b8153838801525
Max Hu <max@12450.top>
2022-01-25 16:17:11 -0500

    change fs read type

--- server.js
+++ server.js
@@ -7,15 +7,18 @@ if (port == undefined) {
     port = 3000;
 }
 
+let content = "";
+fs.readFile('./www/index.html', 'utf8' , (err, data) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  content = data;
+});
+
 http.createServer((request, response) => {
-    fs.readFile('./www/index.html', 'utf8' , (err, data) => {
-        if (err) {
-          console.error(err);
-          process.exit(1);
-        }
-        response.writeHead(200, { 'Content-Type': 'text/html' });
-        response.end(data, 'utf-8');
-      });
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.end(content, 'utf-8');
 }).listen(port);
 
 console.log(`Server listening on port ${port}`);
